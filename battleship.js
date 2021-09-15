cbc59a0c5546aec5c05752e6935dfb910cba0c50
Yasu Flores <carlosyasu91@gmail.com>
2021-09-15 16:41:00 -0500

    catch all for errors on the board positioning

--- battleship.js
+++ battleship.js
@@ -147,32 +147,39 @@ class Battleship {
         )
         );
 
-        this.myFleet.forEach(function (ship) {
-            console.log();
-            console.log(
-                cliColor.yellow(
-                `Please enter the starting position for the ${ship.name} (size: ${ship.size}):`
-                )
-            );
-            const startingPosition = readline.question();
-
-            console.log(
-                cliColor.yellow(
-                `Enter ship orientation: Up (U), Down (D), Right (R), Left (L):`
-                )
-            );
-            const direction = readline.question();
-
-            let nextPosition = Battleship.ParsePosition(startingPosition);
-            for (let i = 0; i < ship.size; i++) {
-                ship.addPosition(nextPosition);
-                nextPosition = nextPosition.getNextPosition(direction.toUpperCase());
+        this.myFleet.forEach((ship) => {
+            try {
+                this.SetupShip(ship);
+            } catch (error) {
+                console.log();
+                console.log();
+                console.log(cliColor.yellow(`Ship placement entered was invalid, please enter a different position or orientation for the ${ship.name} (size:${ship.size}):`));
+                this.SetupShip(ship);
             }
         });
     }
 
     SetupShip(ship) {
+        console.log();
+        console.log(
+            cliColor.yellow(
+            `Please enter the starting position for the ${ship.name} (size: ${ship.size}):`
+            )
+        );
+        const startingPosition = readline.question();
 
+        console.log(
+            cliColor.yellow(
+            `Enter ship orientation: Up (U), Down (D), Right (R), Left (L):`
+            )
+        );
+        const direction = readline.question();
+
+        let nextPosition = Battleship.ParsePosition(startingPosition);
+        for (let i = 0; i < ship.size; i++) {
+            ship.addPosition(nextPosition);
+            nextPosition = nextPosition.getNextPosition(direction.toUpperCase());
+        }
     }
 
     InitializeEnemyFleet() {
